00b1326d290ca695f5a4cd21aff917710790da2e
Mp-keyner <keyneroswaldo0@gmail.com>
2023-09-20 14:08:33 -0500

    20 de septiembre 2:08

--- components/Ras.jsx
+++ components/Ras.jsx
@@ -39,19 +39,32 @@ const MyComponent = () => {
   return (
     <div className="flex items-center justify-center flex-col gap-[1pc] h-[100vh]">
       <div
-        className="w-[23pc] bocadillo-cuadrado5 sm:w-[52pc] h-[12pc] sm:h-[15pc] border-2 text-lg px-4 py-1 z-40 text-[0.6pc]"
+        style={{ width: "23pc" }}
+        className="w-[23pc] bocadillo-cuadrado5 sm:w-[52pc] h-[10pc] sm:h-[15pc] border-2 text-lg px-4 py-1 z-40 text-[0.6pc]"
         data-aos="fade-right" // Definición única de data-aos
         data-aos-out="fade-left"
       >
         <table class="styled-table text-[0.4pc] sm:text-[1pc]">
           <thead>
             <tr>
-              <th>Alegria</th>
-              <th>Enojo</th>
-              <th>Tristeza</th>
-              <th>Miedo</th>
-              <th>Sorpresa</th>
-              <th>Desagrado</th>
+              <th>
+                <p className="w-[2pc]">Alegria</p>
+              </th>
+              <th>
+                <p className="w-[2pc]">Enojo</p>
+              </th>
+              <th>
+                <p className="w-[2pc]">Tristeza</p>
+              </th>
+              <th>
+                <p className="w-[2pc]">Miedo</p>
+              </th>
+              <th>
+                <p className="w-[2pc]">Sorpresa</p>
+              </th>
+              <th>
+                <p className="w-[2pc]">Desagrado</p>
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -61,7 +74,7 @@ const MyComponent = () => {
                   src="https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/documentos%2FcarasBlancas%2F1.png?alt=media&token=8fe4a84f-0e14-4a78-86ba-026d0f88c24c"
                   width={100}
                   height={100}
-                  className="w-[2pc] sm:w-[7pc] alegria"
+                  className="w-[2pc] sm:w-[7pc] alegria border-[3px] border-black rounded-full"
                   title="alegria"
                   onDragStart={(e) =>
                     e.dataTransfer.setData("text/plain", e.target.title)
@@ -73,8 +86,9 @@ const MyComponent = () => {
                 <Image
                   src="https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/documentos%2FcarasBlancas%2F2.png?alt=media&token=c69d47eb-46d2-40f8-baa8-6375aa606731"
                   width={100}
+                  style={{ width: "2pc" }}
                   height={100}
-                  className="w-[2pc] sm:w-[7pc] Enojo"
+                  className="w-[2pc] sm:w-[7pc] Enojo border-[3px] border-black rounded-full"
                   title="Enojo"
                   onDragStart={(e) =>
                     e.dataTransfer.setData("text/plain", e.target.title)
@@ -87,7 +101,7 @@ const MyComponent = () => {
                   src="https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/documentos%2FcarasBlancas%2F3.png?alt=media&token=5388ab3d-868a-402c-bf05-9a230380e5c8"
                   width={100}
                   height={100}
-                  className="w-[2pc] sm:w-[7pc] Tristeza"
+                  className="w-[2pc] sm:w-[7pc] Tristeza border-[3px] border-black rounded-full"
                   title="Tristeza"
                   onDragStart={(e) =>
                     e.dataTransfer.setData("text/plain", e.target.title)
@@ -100,7 +114,7 @@ const MyComponent = () => {
                   src="https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/documentos%2FcarasBlancas%2F4.png?alt=media&token=aa83b80b-06b6-41a9-85ec-ce85d6074041"
                   width={100}
                   height={100}
-                  className="w-[2pc] sm:w-[7pc] Miedo"
+                  className="w-[2pc] sm:w-[7pc] Miedo border-[3px] border-black rounded-full"
                   title="Miedo"
                   onDragStart={(e) =>
                     e.dataTransfer.setData("text/plain", e.target.title)
@@ -113,7 +127,7 @@ const MyComponent = () => {
                   src="https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/documentos%2FcarasBlancas%2F5.png?alt=media&token=0c9373cc-abf3-47c4-a795-1b884645a8a9"
                   width={100}
                   height={100}
-                  className="w-[2pc] sm:w-[7pc] Sorpresa"
+                  className="w-[2pc] sm:w-[7pc] Sorpresa border-[3px] border-black rounded-full"
                   title="Sorpresa"
                   onDragStart={(e) =>
                     e.dataTransfer.setData("text/plain", e.target.title)
@@ -126,7 +140,7 @@ const MyComponent = () => {
                   src="https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/documentos%2FcarasBlancas%2F6.png?alt=media&token=dd373c4e-041d-499a-91cf-2a89b9c327e2"
                   width={100}
                   height={100}
-                  className="w-[2pc] sm:w-[7pc] Desagrado"
+                  className="w-[2pc] sm:w-[7pc] Desagrado border-[3px] border-black rounded-full"
                   title="Desagrado"
                   onDragStart={(e) =>
                     e.dataTransfer.setData("text/plain", e.target.title)
@@ -141,7 +155,7 @@ const MyComponent = () => {
 
       <section className="flex gap-[1pc] flex-wrap items-center justify-center text-center">
         <div
-          className={`w-[25%] text-center p-4 border bg-white border-black rounded-md`}
+          className={`w-[75%] sm:w-[25%] text-center p-4 border bg-white border-black rounded-md`}
         >
           <p>Ganar un examen con excelente nota</p>
           <input
@@ -154,7 +168,7 @@ const MyComponent = () => {
             onChange={(e) => handleInputChange(e, "input1")}
           />
         </div>
-        <div className="w-[25%] text-center p-4 border border-black bg-white rounded-md ">
+        <div className=" w-[75%] sm:w-[25%] text-center p-4 border border-black bg-white rounded-md ">
           <p>Quedarme solo en la oscuridad</p>
           <input
             className="border border-black px-[0.8pc] w-[7pc] text-center bg-transparent"
@@ -166,7 +180,7 @@ const MyComponent = () => {
             onChange={(e) => handleInputChange(e, "input2")}
           />
         </div>
-        <div className="w-[25%] text-center p-4 border border-black bg-white rounded-md ">
+        <div className="w-[75%] sm:w-[25%] text-center p-4 border border-black bg-white rounded-md ">
           <p>Recibir una fiesta sorpresa</p>
           <input
             className="border border-black px-[0.8pc] w-[7pc] text-center bg-transparent"
@@ -178,7 +192,7 @@ const MyComponent = () => {
             onChange={(e) => handleInputChange(e, "input3")}
           />
         </div>
-        <div className="w-[25%] text-center p-4 border border-black bg-white rounded-md ">
+        <div className="w-[75%] sm:w-[25%] text-center p-4 border border-black bg-white rounded-md ">
           <p>Perder a un ser querido</p>
           <input
             className="border border-black px-[0.8pc] w-[7pc] text-center bg-transparent"
@@ -190,7 +204,7 @@ const MyComponent = () => {
             onChange={(e) => handleInputChange(e, "input4")}
           />
         </div>
-        <div className="w-[25%] text-center p-4 border border-black bg-white rounded-md ">
+        <div className="w-[75%] sm:w-[25%] text-center p-4 border border-black bg-white rounded-md ">
           <p>Probar un alimento que no me gusta</p>
           <input
             className="border border-black px-[0.8pc] w-[7pc] text-center bg-transparent"
@@ -202,7 +216,7 @@ const MyComponent = () => {
             onChange={(e) => handleInputChange(e, "input5")}
           />
         </div>
-        <div className="w-[25%] text-center p-4 border border-black bg-white rounded-md ">
+        <div className="w-[75%] sm:w-[25%] text-center p-4 border border-black bg-white rounded-md ">
           <p>No conseguir mi juguete favorito</p>
           <input
             className="border border-black px-[0.8pc] w-[7pc] text-center bg-transparent"

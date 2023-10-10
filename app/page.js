d8f3427ee854286e2d35605e698547a36d3fdcbb
Mp-keyner <keyneroswaldo0@gmail.com>
2023-10-09 19:47:45 -0500

    9 de octubre 7:47

--- app/page.js
+++ app/page.js
@@ -41,6 +41,7 @@ import Fscreen from "@/components/Fscreen";
 import Playo from "@/components/Play";
 import Ras from "@/components/Ras";
 import Slo from "@/components/Slo";
+import Drop from "@/components/Mapa";
 
 export default function Home() {
   const audioSource =
@@ -48,7 +49,7 @@ export default function Home() {
 
   const [Show, setShow] = useState(true);
   const [volumen, setVolumen] = useState(true);
-  const [tal, setTal] = useState(0)
+  const [tal, setTal] = useState(0);
   useEffect(() => {
     AOS.init();
     var isMobile = window.innerWidth <= 768;
@@ -57,118 +58,7 @@ export default function Home() {
   console.log(tal);
   return (
     <>
-      <Playo volumen={volumen} setVolumen={setVolumen} />
-      {Show ? (
-        <Fscreen
-          setShowFunction={setShow}
-          Show={Show}
-          volumen={volumen}
-          setVolumen={setVolumen}
-        />
-      ) : (
-        <>
-          <NO />
-          <div id="Secon">
-            <View1 />
-          </div>
-          <div id="Three">
-            <View2 />
-          </div>
-          <div id="Fourth">
-            <View3 />
-          </div>
-          <div id="Five">
-            <View4 />
-          </div>
-          <div id="Six">
-            <View5 />
-          </div>
-          <div id="Seven">
-            <View6 />
-          </div>
-          <div id="Eight">
-            <View7 />
-          </div>
-          <div id="Nine">
-            <View8 />
-          </div>
-          <div id="ten">
-            <View9 />
-          </div>
-          <div id="Eleven">
-            <View10 />
-          </div>
-          <div id="Twelve">
-            <View11 />
-          </div>
-          <div id="Thirteen">
-            <View12 />
-          </div>
-          <div id="Fourteen">
-            <View13 />
-          </div>
-          <div id="Fifteen">
-            <View14 />
-          </div>
-          <div id="Sixteen">
-            <Slo />
-          </div>
-          <div id="Seventeen">
-            <View16 />
-          </div>
-          <div id="Eightteen">
-            <View17 />
-          </div>
-          <div id="Nineteen">
-            <View18 />
-          </div>
-          <div id="Twenty">
-            <View19 />
-          </div>
-          <div id="TwentyOne">
-            <View20 />
-          </div>
-          <div id="TwentyTwo">
-            <View21 />
-          </div>
-          <div id="TwentyThree">
-            <View22 />
-          </div>
-          <div id="TwentyFour">
-            <View23 />
-          </div>
-          <div id="TwentyFive">
-            <View24 />
-          </div>
-          <div id="TwentySix">
-            <View25 />
-          </div>
-          <div id="TwentySeven">
-            <View26 />
-          </div>
-          <div id="TwentyEight">
-            <View27 />
-          </div>
-          <div id="TwentyNine">
-            <View28 />
-          </div>
-          <div id="Thirty">
-            <View29 />
-          </div>
-          <div id="ThirtyOne">
-            <View30 />
-          </div>
-          <div id="ThirtyTow">
-            <View31 />
-          </div>
-          <div id="ThirtyThree">
-            <View32 />
-          </div>
-          <div id="ThirtyFourt">
-            <View33 />
-          </div>
-        </>
-      )}
+      <Drop />
     </>
   );
 }
